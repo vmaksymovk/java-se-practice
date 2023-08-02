8ebd8b2395a61acdd4843319eee6bd9d8cbc9954
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-08-02 13:42:00 +0200

    homework

--- src/main/java/pl/globallogic/exercises/ex5LeapyearCalcul.java
+++ src/main/java/pl/globallogic/exercises/ex5LeapyearCalcul.java
@@ -2,10 +2,10 @@ package pl.globallogic.exercises;
 
 public class ex5LeapyearCalcul {
     public static void main(String[] args) {
-        System.out.println(isLeapYear(-1600)); // Output: false
-        System.out.println(isLeapYear(1600));  // Output: true
-        System.out.println(isLeapYear(2017));  // Output: false
-        System.out.println(isLeapYear(2000));  // Output: true
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
     }
 
     public static boolean isLeapYear(int year) {
